aa3808d24e5c08e1a98558ecbde125c284c55226
spyder10 <hi.vibhor10@gmail.com>
2021-10-15 23:44:38 +0530

    Error styles added

--- src/Components/CoarseGoals/CourseInput.js
+++ src/Components/CoarseGoals/CourseInput.js
@@ -4,20 +4,30 @@ import { useState } from 'react';
 function CourseInput(props) {
 
     const [enteredCourseGoal,setEnteredCourseGoal] = useState('');
+    const [isvalid,setValid] = useState(true);
 
     const CourseInputChangeHandler = (event) => {
+        setValid(true);
         setEnteredCourseGoal(event.target.value);    
     }
 
     const submitFormHandler = (event) => {
         event.preventDefault();
+        if(enteredCourseGoal.trim().length === 0){
+          setValid(false);
+          return;
+        }
         props.onAddCourseGoal(enteredCourseGoal);
         setEnteredCourseGoal('');
     }
 
+    let classes = "form-control ";
+    if(! isvalid){
+      classes += "invalid";
+    }
   return (
       <form onSubmit={submitFormHandler}>
-        <div className="form-control">
+        <div className={classes}>
           <label>Course Goal</label>
           <input type="text" value={enteredCourseGoal} onChange={CourseInputChangeHandler}></input>
         </div>
